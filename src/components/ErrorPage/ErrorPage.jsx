f7c1b695776c6080807138ff274b7dc6eb8d7ec5
riponmondalbd <riponmondal149@gmail.com>
2024-08-14 02:42:47 +0600

    error page responsive

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import { Link, useRouteError } from "react-router-dom";
 const ErrorPage = () => {
   const error = useRouteError();
   return (
-    <div className="text-center my-[350px] space-y-4">
+    <div className="text-center my-10 lg:my-[300px] space-y-4">
       <h1 className="text-4xl font-extrabold">Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p className="font-extrabold">
