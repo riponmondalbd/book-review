ca8e9aac453ce0f0b576b2580365a1fd36f723cc
riponmondalbd <riponmondal149@gmail.com>
2024-08-14 06:41:17 +0600

    complete book section and also make responsive

---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+
+  return (
+    <div>
+      <div className="text-center mt-[100px] mb-9">
+        <h1 className="font-playFair text-[40px] font-bold">Books</h1>
+      </div>
+
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-3 mb-[120px]">
+        {books.map((book) => (
+          <Book key={book.bookId} book={book}></Book>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Books;
