e15bfcc660b6ec7e87d486d61e32d5a4b68af4ee
riponmondalbd <riponmondal149@gmail.com>
2024-08-14 06:45:12 +0600

    add footer

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
   return (
@@ -8,6 +9,7 @@ const Root = () => {
         <Navbar />
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
