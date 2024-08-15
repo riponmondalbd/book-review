da68db132359f99a39481bcdc8dd57c406db9105
riponmondalbd <riponmondal149@gmail.com>
2024-08-15 07:23:24 +0600

    book page header design

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,7 +1,9 @@
 const ListedBooks = () => {
   return (
-    <div>
-      <h1>hello from listed books</h1>
+    <div className="px-[7px] mt-[36px]">
+      <div className="text-center py-[34px] bg-[#1313130D] rounded-lg mb-8">
+        <h1 className="text-[28px] font-bold">Books</h1>
+      </div>
     </div>
   );
 };
